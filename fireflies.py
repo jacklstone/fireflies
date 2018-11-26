838c801a8ab96c9d65d0ab51adf52bf93d46465f
jaclynstone <44078085+jaclynstone@users.noreply.github.com>
2018-11-26 13:34:15 -0800

    Update fireflies.py
    Changed coloring of the pulses
    Changed increment for each oscillator to make sure individual fireflies can pulse

--- fireflies.py
+++ fireflies.py
@@ -15,22 +15,21 @@ def inverseDistance(t, j, loc, disDict):
 	return invDis
 
 
-def drawImage(y, x, loc, r):
+def drawImage(y, x, loc, r, dropOff):
 	img=np.zeros((x,y,3)).astype(np.uint8)
 	for c in loc:
-		value = (1 - c[2] / c[3])
+		value = (1 - c[2] / c[3]) * (1+dropOff*r)
 		for i in range(r-1, -1, -1):
-			temp = value - .04*i
+			temp = value - dropOff*i
 			if temp > 0:
-				color = (255*2*(temp-0.5), 255, 255) if temp > 0.5 else (0, 255 * 2 * temp, 255 * 2 * temp)
+				color = (min(255,255*2*(temp-.5)), 255, 255) if temp > .5 else (0, 255 * 2 * (temp), 255 * 2 * (temp))
 				cv2.circle(img, (int(c[0]), int(c[1])), i+1, color, -1)
 	cv2.imshow("Fireflies",img)
 	k = cv2.waitKey(1)
 	if (k == 27 or k == ord('q')):
 		return 1
 	return 0
-
-def run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, epsilon, convergenceFactor):
+def run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, convergenceFactor, dropOff):
 	maxDist = x**2 + y**2
 
 	loc = createLocations(x, y, z, threshold, r)
@@ -38,6 +37,7 @@ def run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, epsil
 
 	movementPhase = [0 for t in loc]
 	target = [(t[0],t[1]) for t in loc]
+	maxThresh = threshold+periodRange/2
 
 	tick = 0
 	flag = 0
@@ -64,14 +64,14 @@ def run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, epsil
 			if minPeriod/maxPeriod > convergenceFactor: 
 				print("Periods equal")
 				flag = 1
-		popped = [(loc[t][2] + (threshold-loc[t][2]) * alpha) > loc[t][3] for t in range(len(loc))]
+		popped = [(loc[t][2] +(maxThresh-loc[t][2]) * alpha) > loc[t][3] for t in range(len(loc))]
 		
 		for t in range(len(loc)):
 			if (movementPhase[t] != 0):
 				loc[t][0] = (loc[t][0] + (movementPhase[t] * (target[t][0] - loc[t][0])) //moveDuration) % x
 				loc[t][1] = (loc[t][1] + (movementPhase[t] * (target[t][1] - loc[t][1])) //moveDuration) % y
 				movementPhase[t] = (movementPhase[t] + 1) % moveDuration
-			loc[t][2]+=(threshold-loc[t][2]) * alpha
+			loc[t][2]+=(maxThresh-loc[t][2]) * alpha
 			if popped[t]:
 				for j in range(len(loc)):
 					if t != j and not popped[j]:
@@ -82,7 +82,7 @@ def run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, epsil
 							loc[j][3] += (b * invDis * sin(loc[t][3] - loc[j][3]))
 				loc[t][2] = 0
 		tick+=1
-		if (drawImage(x, y, loc, r)):
+		if (drawImage(x, y, loc, r, dropOff)):
 			break
 
 x = 1280
@@ -90,16 +90,16 @@ y = 700
 z = 100
 r = 5
 
-b = 1.0
+b = 200/z
 alpha = pi/20
 threshold = 2*pi
 periodRange = .4
 
-stillness = .01
+stillness = 1/z
 maxMove = 100
 moveDuration = 10
 
-epsilon = 10
 convergenceFactor = .9995
+dropOff = .16
 
-run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, epsilon, convergenceFactor)
+run(x, y, z, r, b, alpha, threshold, stillness, maxMove, moveDuration, convergenceFactor, dropOff)
